9a1eab6c70bb5d8285c6750c9fcf7efed7e55d0b
John Nicholas <john.nicholas@dinoroar.io>
2019-05-07 21:53:52 +0100

    pointless lint

--- lib/json_parser.ex
+++ lib/json_parser.ex
@@ -5,23 +5,34 @@ defmodule JsonParser do
 
   def get_reasons(filename) do
     IO.inspect filename
-    |> get_json
-    |> get_failures
+               |> get_json
+               |> get_failures
   end
 
   defp get_reason(_ = %{"errors" => errors}) do
     Enum.map(errors, fn e -> e["reason"]  end)
   end
 
-  defp get_failures(%{"products" => %{"failures" => failures}}) do
+  defp get_failures(
+         %{
+           "products" => %{
+             "failures" => failures
+           }
+         }
+       ) do
     Enum.map(failures, fn f -> get_reason(f) end)
   end
 
+  defp get_failures(err) do
+    err end
+
   defp get_json(filename) do
     with {:ok, body} <- File.read(filename),
-         {:ok, json} <- Poison.decode(body), do: {:ok, json}
+         {:ok, json} <- Poison.decode(body)
       do
       json
+    else
+      err -> err
     end
   end
 end
